6b9e3f9d6259438468c3ea256097d4b4c9b2ea02
nkiyo <nakahara.kiyoshi@gmail.com>
2024-03-27 08:59:30 +0900

    up

--- filter_log.py
+++ filter_log.py
@@ -1,3 +1,25 @@
+import sys
 import glob
+import re
+from datetime import datetime
 
-w
+if len(sys.argv) != 3:
+    print(f"Usage: Python3 {sys.argv[0]} <start_time> <end_time>")
+    sys.exit(1)
+s = datetime.strptime(sys.argv[1], "%Y-%m-%d %H:%M:%S")
+e = datetime.strptime(sys.argv[2], "%Y-%m-%d %H:%M:%S")
+
+files = glob.glob("log/log.*")
+# print(files)
+
+for file in files:
+    with open(file, 'r') as f:
+        for line in f:
+            pattern = r"^\[(.*)\].*$"
+            match = re.search(pattern, line)
+
+            if match:
+                time = datetime.strptime(match.group(1), "%Y-%m-%d %H:%M:%S.%f")
+                if s < time < e:
+                    print(time)
+            
